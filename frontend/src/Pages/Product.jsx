721ec40d85a322ba8de75f27f8d91032a10b681f
yugaldhiman <yugaldhiman14@gmail.com>
2025-05-16 11:18:25 +0530

    resolve error in product.jsx

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,63 +1,29 @@
-import React, { useContext } from 'react'
-import { useParams } from 'react-router-dom'
-import { ShopContext } from '../Context/ShopContext'
-import Breadcrum from '../Component/Breadcrum/Breadcrum'
-import ProductDisplay from '../Component/ProductDisplay/ProductDisplay'
-import Discrip from '../Component/Descriptionbox/Discrip'
-import Related from '../Component/RealatedProduct/Related'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext';
+import Breadcrum from '../Component/Breadcrum/Breadcrum';
+import ProductDisplay from '../Component/ProductDisplay/ProductDisplay';
+import Discrip from '../Component/Descriptionbox/Discrip';
+import Related from '../Component/RealatedProduct/Related';
 
 function Product() {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
 
-  const { all_product } = useContext(ShopContext)
-  const { productId } = useParams()
-  const product = all_product.find((e) => e.id === Number(productId))
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  if (!product) {
+    return <div style={{ padding: "2rem", fontSize: "18px" }}>Loading product details...</div>;
+  }
 
   return (
-    <>
-      <div >
-        <Breadcrum product={product} />
-        <ProductDisplay product={product} />
-        <Discrip />
-        <Related currentProduct={product} />
-      </div>
-    </>
-
-  )
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <Discrip />
+      <Related currentProduct={product} />
+    </div>
+  );
 }
 
-export default Product
-
-
-// import React, { useContext } from 'react'
-// import { useParams, Navigate } from 'react-router-dom'
-// import { ShopContext } from '../Context/ShopContext'
-// import Breadcrum from '../Component/Breadcrum/Breadcrum'
-// import ProductDisplay from '../Component/ProductDisplay/ProductDisplay'
-// import Discrip from '../Component/Descriptionbox/Discrip'
-// import Related from '../Component/RealatedProduct/Related'
-
-// function Product() {
-//   const { all_product, loading } = useContext(ShopContext) // assuming you have loading state
-//   const { productId } = useParams()
-
-//   if (loading) {
-//     return <div>Loading product...</div>
-//   }
-
-//   const product = all_product?.find((e) => e.id === Number(productId))
-
-//   if (!product) {
-//     return <Navigate to="/" />
-//   }
-
-//   return (
-//     <main>
-//       <Breadcrum product={product} />
-//       <ProductDisplay product={product} />
-//       <Discrip />
-//       <Related currentProduct={product} />
-//     </main>
-//   )
-// }
-
-// export default Product
+export default Product;
